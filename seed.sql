05c5fcbb76a455df40f0e035e9a380c11fa840d4
Zach Blume <zachblume@gmail.com>
2023-09-25 22:36:26 -0400

    This should pass but doesn't

--- seed.sql
+++ seed.sql
@@ -1,30 +1,89 @@
 -- Setup for postgrest
 \set pgpass `echo "$POSTGRES_PASSWORD"`;
 
+-- a placeholder/mock function
+CREATE FUNCTION public.requesting_org_id() RETURNS text AS $$
+    SELECT '00000000-0000-0000-0000-000000000007'::text;
+$$ LANGUAGE sql;
+
+-- a placeholder/mock function
+CREATE FUNCTION public.requesting_user_id() RETURNS text AS $$
+    SELECT '00000000-0000-0000-0000-000000000008'::text;
+$$ LANGUAGE sql;
+
 -- Create the people table
-CREATE TABLE people (
-    person_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
-);
+create table
+  public.people (
+    batch_id uuid null,
+    id uuid not null default gen_random_uuid (),
+    created_at timestamp with time zone not null default current_timestamp,
+    inserted_at timestamp with time zone not null default current_timestamp,
+    updated_at timestamp with time zone not null default current_timestamp,
+    organization_id text not null default requesting_org_id (),
+    first_name text null,
+    last_name text null,
+    addr1 text null,
+    addr2 text null,
+    city text null,
+    state text null,
+    zip text null,
+    country text null,
+    occupation text null,
+    employer text null,
+    custom_field_1 text null,
+    custom_field_2 text null,
+    custom_field_3 text null,
+    bio text null,
+    current_ask_amount integer null,
+    name_concat text null,
+    user_id text null default requesting_user_id (),
+    donor_research text null,
+    -- numeric_id integer not null default nextval('people_numeric_id_seq'::regclass),
+    constraint people_pkey primary key (id)
+  ) tablespace pg_default;
 
--- Create the interactions table
-CREATE TABLE interactions (
-    interaction_id SERIAL PRIMARY KEY,
-    person_id INT REFERENCES people(person_id), -- Foreign Key referencing the people table
-    notes TEXT
-);
+create table
+  public.interactions (
+    id uuid not null default gen_random_uuid (),
+    created_at timestamp with time zone not null default current_timestamp,
+    inserted_at timestamp with time zone not null default current_timestamp,
+    updated_at timestamp without time zone null,
+    batch_id uuid null,
+    person_id uuid not null,
+    contact_type text null,
+    disposition text null,
+    disposition_more text null,
+    resulted_in_pledge boolean not null default false,
+    pledge_id uuid null,
+    note text null,
+    organization_id text not null default requesting_org_id (),
+    call_sid text null,
+    ended_at timestamp with time zone null,
+    call_session_id bigint null,
+    phone_number_id uuid null,
+    user_id text null default requesting_user_id (),
+    constraint interactions_pkey primary key (id),
+    constraint interactions_person_id_fkey foreign key (person_id) references people (id)
+    -- constraint interactions_call_session_id_fkey foreign key (call_session_id) references call_sessions (id),
+    -- constraint interactions_pledge_id_fkey foreign key (pledge_id) references pledges (id),
+    -- constraint interactions_phone_number_id_fkey foreign key (phone_number_id) references phone_numbers (id)
+  ) tablespace pg_default;
 
--- Inserting example data into the people table
-INSERT INTO people (first_name, last_name) VALUES ('John', 'Doe');
-INSERT INTO people (first_name, last_name) VALUES ('Jane', 'Smith');
-INSERT INTO people (first_name, last_name) VALUES ('Alice', 'Johnson');
-INSERT INTO people (first_name, last_name) VALUES ('Bob', 'Williams');
-INSERT INTO people (first_name, last_name) VALUES ('Charlie', 'Brown');
+--   Some examples in people and interactions for testing
+insert into people (id, first_name, last_name, addr1, addr2, city, state, zip, country, occupation, employer, custom_field_1, custom_field_2, custom_field_3, bio, current_ask_amount, donor_research, name_concat)
+values ('00000000-0000-0000-0000-000000000001', 'John', 'Doe', '1 Main St', 'Apt 1', 'Anytown', 'NY', '12345', 'USA', 'Software Engineer', 'Acme Corp', 'Custom 1', 'Custom 2', 'Custom 3', 'This is a bio', 100, 'This is donor research', 'John Doe');
 
--- Inserting example data into the interactions table
-INSERT INTO interactions (person_id, notes) VALUES (1, 'Met at a conference.');
-INSERT INTO interactions (person_id, notes) VALUES (2, 'Had a phone call regarding the project.');
-INSERT INTO interactions (person_id, notes) VALUES (3, 'Sent an email about the upcoming meeting.');
-INSERT INTO interactions (person_id, notes) VALUES (4, 'Discussed the new proposal.');
-INSERT INTO interactions (person_id, notes) VALUES (5, 'Met for coffee to discuss collaboration.');
+insert into people (id, first_name, last_name, addr1, addr2, city, state, zip, country, occupation, employer, custom_field_1, custom_field_2, custom_field_3, bio, current_ask_amount, donor_research, name_concat)
+values ('00000000-0000-0000-0000-000000000002','Jane', 'Doe', '2 Main St', 'Apt 2', 'Anytown', 'NY', '12345', 'USA', 'Software Engineer', 'Acme Corp', 'Custom 1', 'Custom 2', 'Custom 3', 'This is a bio', 100, 'This is donor research', 'Jane Doe');
+
+insert into people (id, first_name, last_name, addr1, addr2, city, state, zip, country, occupation, employer, custom_field_1, custom_field_2, custom_field_3, bio, current_ask_amount, donor_research, name_concat)
+values ('00000000-0000-0000-0000-000000000003','John', 'Smith', '3 Main St', 'Apt 3', 'Anytown', 'NY', '12345', 'USA', 'Software Engineer', 'Acme Corp', 'Custom 1', 'Custom 2', 'Custom 3', 'This is a bio', 100, 'This is donor research', 'John Smith');
+
+insert into interactions (person_id, contact_type, disposition, resulted_in_pledge, note)
+values ('00000000-0000-0000-0000-000000000001', 'Call', 'Left Voicemail', false, 'This is a note 1');
+
+insert into interactions (person_id, contact_type, disposition, resulted_in_pledge, note)
+values ('00000000-0000-0000-0000-000000000001', 'Call', 'Left Voicemail', false, 'This is a note 2');
+
+insert into interactions (person_id, contact_type, disposition, resulted_in_pledge, note)
+values ('00000000-0000-0000-0000-000000000002', 'Call', 'Left Voicemail', false, 'This is a note 3');
--- test.js
+++ test.js
@@ -4,13 +4,21 @@ describe("My issue", () => {
       "http://localhost:3000/interactions?select=*,people(*)&people.or=(first_name.ilike.%John%)&or=(people.not.is.null)";
     const result = await fetch(url);
     const json = await result.json();
-    expect(json).toEqual([]);
+    expect(json.length).toBeGreaterThan(0);
   });
   test("minimal", async () => {
     const url =
       "http://localhost:3000/interactions?select=*,people(*)&people=not.is.null";
     const result = await fetch(url);
     const json = await result.json();
-    expect(json).toEqual([]);
+    expect(json.length).toBeGreaterThan(0);
+  });
+  // http://localhost:8000/rest/v1/interactions?select=*,people(*)&or=(people.not.is.null)
+  test("minimal with or", async () => {
+    const url =
+      "http://localhost:3000/interactions?select=*,people(*)&or=(people.not.is.null)";
+    const result = await fetch(url);
+    const json = await result.json();
+    expect(json.length).toBeGreaterThan(0);
   });
 });
